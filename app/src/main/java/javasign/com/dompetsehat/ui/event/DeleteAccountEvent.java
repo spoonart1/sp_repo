7e13f88e8e93279dcb9157300aacc3fea902cfbd
lafran <lafran@docotel.co.id>
2017-11-17 12:45:47 +0700

    big deletion

--- app/src/main/java/javasign/com/dompetsehat/ui/event/DeleteAccountEvent.java
+++
@@ -1,8 +0,0 @@
-package javasign.com.dompetsehat.ui.event;
-
-/**
- * Created by lafran on 1/25/17.
- */
-
-public class DeleteAccountEvent {
-}
--- app/src/main/java/javasign/com/dompetsehat/ui/event/SharingDataEvent.java
+++
@@ -1,8 +0,0 @@
-package javasign.com.dompetsehat.ui.event;
-
-/**
- * Created by lafran on 1/4/17.
- */
-
-public class SharingDataEvent {
-}
